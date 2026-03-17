a5dc80cab9f73bd029de4cd696d45347b4286735
andreibulankou <C28Andrei.Bulankou@afacademy.af.edu>
2026-03-16 21:27:57 -0600

    finished code

--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -86,18 +86,58 @@ end top_basys3;
 architecture top_basys3_arch of top_basys3 is 
   
 	-- declare components
-
+    component thunderbird_fsm is 
+     port (
+        i_clk, i_reset  : in    std_logic;
+        i_left, i_right : in    std_logic;
+        o_lights_L      : out   std_logic_vector(2 downto 0);
+        o_lights_R      : out   std_logic_vector(2 downto 0)
+    );
+  end component;
   
+  component clock_divider is
+    generic ( constant k_DIV : natural := 2	); -- How many clk cycles until slow clock toggles
+											   -- Effectively, you divide the clk double this 
+											   -- number (e.g., k_DIV := 2 --> clock divider of 4)
+	port ( 	i_clk    : in std_logic;
+			i_reset  : in std_logic;		   -- asynchronous
+			o_clk    : out std_logic		   -- divided (slow) clock
+	);
+	end component;
+	signal w_clkin: std_logic;
+    signal w_leftlight: std_logic_vector(2 downto 0);
+    signal w_rightlight: std_logic_vector(2 downto 0);
 begin
 	-- PORT MAPS ----------------------------------------
-
+	thunderbird_inst: thunderbird_fsm
+	   port map (
+	       i_clk => w_clkin,
+	       i_reset => btnR,
+	       i_left => sw(15),
+	       i_right => sw(0),
+	       o_lights_L => w_leftlight,
+	       o_lights_R => w_rightlight
+	   
+	   
+	   );
+    clkdiv_inst : clock_divider 		--instantiation of clock_divider to take 
+        generic map ( k_DIV => 12500000 ) -- 1 Hz clock from 100 MHz
+        port map (						  
+            i_clk   => clk,
+            i_reset => btnL,
+            o_clk   => w_clkin
+        );    
+        
 	
 	
 	-- CONCURRENT STATEMENTS ----------------------------
-	
+	led(15 downto 13) <= w_leftlight;
+	--right light is "backwards", map manually
+	led(2) <= w_rightlight(0);
+	led(1) <= w_rightlight(1);
+	led(0) <= w_rightlight(2);
 	-- ground unused LEDs
 	-- leave unused switches UNCONNECTED
-	
 	-- Ignore the warnings associated with these signals
 	-- Alternatively, you can create a different board implementation, 
 	--   or make additional adjustments to the constraints file
